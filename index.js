9b914b6e7a375107c39f04c0d2ff58546354ea84
Nick Ellsworth <nellsworth@spotify.com>
2020-11-09 13:29:43 -0500

    this is silly

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const nameInput = document.getElementById('name')
 
 const desiredString = "Stupid Idiot"
 
-const normalizeInput = (text) => {
-
-}
-
 nameInput.addEventListener('input', e => {
-  const nextInput = e.target.value
-  const nextLength = nextInput.length
+  const nextLength = e.target.value.length
   nameInput.value = desiredString.substring(0, nextLength)
 })
